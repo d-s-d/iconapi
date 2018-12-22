c2d0b0b8edb75f2176ea6b2be699ec31ac98eff1
Daniel S. Dietiker <stefan.dietiker@gmail.com>
2018-12-22 23:09:23 +0100

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![deny(warnings)]
+extern crate warp;
+
+use warp::Filter;
+
 fn main() {
-    println!("Hello, world!");
+    // Match any request and return hello world!
+    let routes = warp::any()
+        .map(|| "Hello, World!");
+
+    println!("starting server.");
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030));
 }
+
